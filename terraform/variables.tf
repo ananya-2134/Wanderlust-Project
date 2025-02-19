5cb689fd6b7fce9dfa98bf6969d153aaa0f9f847
Ubuntu <ubuntu@ip-172-31-87-23.ec2.internal>
2025-02-19 09:45:46 +0000

    Initial commit

---
+++ terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "aws_region" {
+  description = "AWS region where resources will be provisioned"
+  default     = "us-east-1"
+}
+
+variable "ami_id" {
+  description = "AMI ID for the EC2 instance"
+  default     = "ami-04b4f1a9cf54c11d0"
+}
+
+variable "instance_type" {
+  description = "Instance type for the EC2 instance"
+  default     = "t2.large"
+}
